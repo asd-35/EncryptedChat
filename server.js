d5f21affd4cfaed2e80c3cc050e619b1a0039da1
asd-35 <saridoganarda35@gmail.com>
2021-05-25 00:59:07 +0300

    roputing added, logni is a form password is being encrypted

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
+
+
 app.set("view engine","ejs");
 
 app.listen(3000);
@@ -13,18 +15,29 @@ app.listen(3000);
 console.log("live on 3000")
 
 app.get("/", (req,res) => {
+	
 	res.render("homepage")
 	});
 
+app.post("/", (req,res) => {
+	
+	res.redirect("/login");
+});
+
 app.get("/chat", (req,res) => {
 	res.render("chat")
-	//console.log(CryptoJS.SHA256("seed").toString(CryptoJS.enc.Hex))
+	
 	// SHA-256 will use later
 	});
 
 app.get("/login", (req,res) => {
 	res.render("login")
 	
+	});
+
+app.post("/login", (req,res) => {
+	res.redirect("chat")
+	console.log(CryptoJS.SHA256(req.body.pass).toString(CryptoJS.enc.Hex))
 	});
 
 app.use((req,res) => {
