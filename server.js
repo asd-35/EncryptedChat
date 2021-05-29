01bb931ac9e13fffb5d4cffb3a4b604ce8cdcf02
asd-35 <saridoganarda35@gmail.com>
2021-05-29 05:27:21 +0300

    jwt auth added

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const CryptoJS = require("crypto-js");
 const mysql = require('mysql2');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
 
 
 const connection = mysql.createConnection({
@@ -14,6 +17,33 @@ const connection = mysql.createConnection({
   password: "Passw0rd!"
 }); 
 
+const secret = "fb2eb10425b3816df517314443c0d98f487779ce02f4440baee81286484e0962dfb32541b76716360564ac4c92eccc6a7cbd7e7d25e87e5e5bb07d4802a04517"
+//secret created by crypto.randomBytes(64).toString(hex)
+const time = 3 * 24 * 60 * 60;
+
+const createToken = (mail) => {
+	return jwt.sign({ mail }, secret,
+		{
+			expiresIn: time
+		})
+	
+}
+
+const requireAuth = (req,res,next) => {
+	const token = req.cookies["session-token"]
+
+	if(token){
+		jwt.verify(token,secret,(err,decoded) =>{
+			if(err){
+				res.redirect("/login");
+			}else{
+				next();
+			}
+		})
+	}else{
+		res.redirect("/login");
+	}
+}
 
 app.set("view engine","ejs");
 
@@ -31,7 +61,7 @@ app.post("/", (req,res) => {
 	res.redirect("/login");
 });
 
-app.get("/chat", (req,res) => {
+app.get("/chat",requireAuth ,(req,res) => {
 	res.render("chat")
 	
 	
@@ -54,6 +84,8 @@ app.post("/login", (req,res) => {
   'SELECT * FROM Users WHERE username = "'+ user +'" AND user_password = "' + pass + '"',
   function(err, results, fields) {
     if(results != 0){
+    	const token = createToken(user);
+    	res.cookie("session-token",token,{httpOnly: true, maxAge: time * 1000})
     	res.redirect("chat");
     	
     }else{
